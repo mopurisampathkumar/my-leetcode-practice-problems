3ad55d7457787e3abc57cebc4b78e497810e71f1
Mopuri Sampath kumar <147181744+mopurisampathkumar@users.noreply.github.com>
2025-05-19 07:49:43 +0530

    Create file

---
+++ 3024-TypeofTriangle/3024-TypeofTriangle.java
@@ -0,0 +1,41 @@
+// Last updated: 5/19/2025, 7:49:06 AM
+class Solution {
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+    private void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+    public String triangleType(int[] nums) {
+        quickSort(nums,0,nums.length - 1);
+        if(nums[0] + nums[1] <= nums[2]) return "none";
+
+        if(nums[0] == nums[1] && nums[0] == nums[2]) return "equilateral";
+        else if(nums[0] == nums[1] && nums[0] != nums[2]) return "isosceles";
+        else if(nums[0] != nums[1] && nums[1] == nums[2]) return "isosceles";
+        else if(nums[0] != nums[1] && nums[0] == nums[2]) return "isosceles";
+        else return "scalene";
+    }
+    
+}
