a29f34aa24acd0dc6fb4c93f83d3ef5b687f3aa6
Mopuri Sampath kumar <147181744+mopurisampathkumar@users.noreply.github.com>
2026-01-05 01:15:44 +0530

    Time: 0 ms (100%), Space: 78.5 MB (14%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,28 +1,29 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // int curr = nums[0],maxsum = nums[0];
-        // for(int i =1;i < nums.length;i++){
-        //     curr= Math.max(nums[i],curr+nums[i]);
-        //     maxsum = Math.max(curr,maxsum);
-        // }
-        // return maxsum;
-
-       int n = nums.length;
-        int sum = 0;
-        int max = Integer.MIN_VALUE;
-        for(int i  =0;i<n;i++){
-            sum += nums[i];
-            if(sum > max){
-                max = sum;
-            }
-            if(sum < 0){
-                sum = 0;
-            }
+        int curr = nums[0],maxsum = nums[0];
+        for(int i =1;i < nums.length;i++){
+            curr= Math.max(nums[i],curr+nums[i]);
+            maxsum = Math.max(curr,maxsum);
         }
-        return max;
+        return maxsum;
+
+    //    int n = nums.length;
+    //     int sum = 0;
+    //     int max = Integer.MIN_VALUE;
+    //     for(int i  =0;i<n;i++){
+    //         sum += nums[i];
+    //         if(sum > max){
+    //             max = sum;
+    //         }
+    //         if(sum < 0){
+    //             sum = 0;
+    //         }
+    //     }
+    //     return max;
 
 
     }
+
     static {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
